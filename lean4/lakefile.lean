d9539954d6cf034b77a16bdb36f6100d45065b1f
utensil <utensilcandel@gmail.com>
2022-07-18 19:48:54 +0800

    Check Lean 4 examples in CI
    Verified locally by https://github.com/nektos/act

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -1,5 +1,5 @@
 import Lake
-open Lake DSL
+open Lake DSL System
 
 package hello {
   -- add package configuration options here
@@ -13,3 +13,37 @@ lean_lib Hello {
 lean_exe hello {
   root := `Main
 }
+
+script check_examples (args) do
+  let cwd ← IO.currentDir
+
+  let mut total := 0
+  let mut failed : Array (FilePath × IO.Process.Output) := #[]
+
+  for ex in (← (cwd / "examples").readDir) do
+    if ex.path.extension == some "lean" then
+      total := total + 1
+
+      let r ← timeit s!"Running example: {ex.fileName}\t" (IO.Process.output {
+        cmd := "lean"
+        args := #[ex.path.toString]
+        env := #[] -- #[("LEAN_PATH", searchPath)]
+      })
+    
+      if r.exitCode == 0 then
+        IO.println "  Success!🟢"
+      else
+        failed := failed.append #[(ex.path, r)]
+        IO.println "  Failed!🔴"
+
+  if failed.size != 0 then 
+    IO.println "\nFailed examples:"
+    for (ex, _) in failed do
+      IO.println s!"  {ex}"
+  
+  let succeeded := total - failed.size
+  let resultMarker := if succeeded == total then "✅" else "❌"
+
+  IO.println s!"\nSuccessful examples: {succeeded} / {total} {resultMarker}"
+
+  return 0
