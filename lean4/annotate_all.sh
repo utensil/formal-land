bfe6ef5ee31b66b23e141a390ecaa6864fdf8aae
Utensil <utensilcandel@gmail.com>
2023-09-07 11:19:23 +0000

    Try fix lean 4 CI

--- lean4/annotate_all.sh
+++ lean4/annotate_all.sh
@@ -7,6 +7,9 @@ set -o pipefail
 # Install dependencies first
 # ./install.sh
 
-# TODO reproduce the problem of not specifying `--lake` and file an issue
+# TODO Warn if /tmp/leanInk/build/bin/ doesn't exist
+export PATH=/tmp/leanInk/build/bin/:$HOME/.elan/bin:$PATH
+
 alectryon examples/Hello.lean --lake lakefile.lean --output-directory dist/
 alectryon examples/LAMR.lean --lake lakefile.lean --output-directory dist/
+alectryon examples/Tactics.lean --lake lakefile.lean --output-directory dist/
