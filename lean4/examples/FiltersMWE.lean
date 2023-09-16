73a92d46419b044e016eeb1605fbd0c04fab144a
utensil <utensilcandel@gmail.com>
2023-09-16 23:33:24 +0800

    Explore filter proofs with the hint from #explode

--- lean4/examples/FiltersMWE.lean
+++ lean4/examples/FiltersMWE.lean
@@ -3,10 +3,10 @@ import Mathlib.Data.Set.Finite
 import Mathlib.Order.Filter.Basic
 import Mathlib.Tactic.Explode
 
-theorem Filter.ext_iff' (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
+theorem Filter.ext_iff₁ (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
   simp only [filter_eq_iff, Set.ext_iff, Filter.mem_sets]
 
-theorem Filter.ext_iff'' (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
+theorem Filter.ext_iff₂ (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
   apply Iff.intro
   . intro f_eq_g
     intro s
@@ -24,9 +24,36 @@ theorem Filter.ext_iff'' (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈
     specialize set_mem_iff s
     exact set_mem_iff
 
-#explode Filter.ext_iff'
+theorem Filter.ext_iff₃ (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
+  calc
+    (f = g) ↔ (f.sets = g.sets)                           := by rw [filter_eq_iff]
+          _ ↔ ∀ (x : Set α), x ∈ f.sets ↔ x ∈ g.sets      := by rw [Set.ext_iff]
+          _ ↔ ∀ (x : Set α), x ∈ f ↔ x ∈ g                := by simp_rw [Filter.mem_sets]
 
-#explode Filter.ext_iff''
+theorem Filter.ext_iff₄ (f g : Filter α) : f = g ↔ (∀ s, s ∈ f ↔ s ∈ g) := by
+  have mem_sets_eq_mem: ∀ (fg : Filter α) (x : Set α), (x ∈ fg.sets) = (x ∈ fg) := by
+    intros fg x
+    rw [Filter.mem_sets]
+  calc
+    (f = g) ↔ (f.sets = g.sets)                           := by rw [filter_eq_iff]
+          _ ↔ ∀ (x : Set α), x ∈ f.sets ↔ x ∈ g.sets      := by rw [Set.ext_iff]
+          _ ↔ ∀ (x : Set α), x ∈ f ↔ x ∈ g                := by
+            conv_lhs =>
+              intro x
+              rw [mem_sets_eq_mem f]
+              rw [mem_sets_eq_mem g]
+
+-- 19
+#explode Filter.ext_iff₁
+
+-- 38
+#explode Filter.ext_iff₂
+
+-- 31
+#explode Filter.ext_iff₃
+
+-- 60
+#explode Filter.ext_iff₄
 
 def f : α → α :=
   fun f =>
