87a2d688af903909d7d0a049cd8b33c1670fa73d
utensil <utensilcandel@gmail.com>
2023-11-02 13:29:34 +0800

    Make MWE of crash issue

--- lean4/examples/Help.lean
+++ lean4/examples/Help.lean
@@ -1,3 +1,5 @@
-import Mathlib.Tactic
+-- https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/LeanInk.20regression.20on.20v4.2E3.2E0-rc1
 
-#help tactic
+import Mathlib.Tactic.Relation.Symm
+
+attribute [symm] Eq.symm
