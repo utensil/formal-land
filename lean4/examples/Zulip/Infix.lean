b0dcca4383ec74847f281d751140f792bc833b45
utensil <utensilcandel@gmail.com>
2023-12-09 17:08:49 +0800

    Infix delab

---
+++ lean4/examples/Zulip/Infix.lean
@@ -0,0 +1,14 @@
+-- https://leanprover.zulipchat.com/#narrow/stream/113489-new-members/topic/Overloading.20infixes.3F
+import Mathlib.Tactic.Common
+import Mathlib.Mathport.Notation
+
+notation3:70 x " ⊗₁ " y => x * y
+infix:70 " ⊗₂ " => Mul.mul
+
+variable (Φ : Type) [Mul Φ] (φ ψ : Φ)
+
+/-- info: φ ⊗₁ ψ : Φ -/
+#guard_msgs in #check φ ⊗₁ ψ
+
+/-- info: φ ⊗₂ ψ : Φ -/
+#guard_msgs in #check φ ⊗₂ ψ
