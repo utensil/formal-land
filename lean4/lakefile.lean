69c9e191d68a4bb6a9b1c8b17718c5699e34495c
utensil <utensilcandel@gmail.com>
2024-02-04 15:39:45 +0800

    Bump to v4.6.0-rc1
    and disable annotations for now

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -6,7 +6,7 @@ package «hello» {}
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "main"
 
 @[default_target]
 lean_lib «Hello» {
