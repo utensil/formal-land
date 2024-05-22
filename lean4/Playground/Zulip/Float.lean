9188be68974023d153be035abae05e0410ef75a9
utensil <utensilcandel@gmail.com>
2024-05-22 16:50:53 +0800

    Extract fillLeft

--- lean4/Playground/Zulip/Float.lean
+++ lean4/Playground/Zulip/Float.lean
@@ -1,5 +1,12 @@
 import Mathlib.Algebra.Group.Nat
 
+namespace String
+
+def fillLeft (s : String) (c : Char) (n : Nat)  : String :=
+  "".pushn c (n - s.length) ++ s
+
+end String
+
 namespace Float
 
 def intPart (f : Float) : String :=
@@ -9,33 +16,28 @@ def toDecimal (f : Float) (precision : Nat) : String :=
   let int := f.round
   let intStr := int.intPart
   let fraction := ((f-int) * (10 ^ precision).toFloat).abs.round
-  let fractionStr := fraction.intPart
-  let fractionStrFinal := "".pushn '0' (precision - fractionStr.length) ++ fractionStr
-  if precision > 0 then s!"{intStr}.{fractionStrFinal}" else intStr
+  let fractionStr := fraction.intPart.fillLeft '0' precision
+  if precision > 0 then s!"{intStr}.{fractionStr}" else intStr
 
 end Float
 
-/-
-"3.11235"
--/
+
+/-- info: "3.11235" -/
+#guard_msgs in
 #eval 3.1123456.toDecimal 5
 
-/-
-"-123.4567890"
--/
+/-- info: "-123.4567890" -/
+#guard_msgs in
 #eval (-123.456789).toDecimal 7
 
-/-
-"0.0000100"
--/
+/-- info: "0.0000100" -/
+#guard_msgs in
 #eval (0.00001).toDecimal 7
 
-/-
-"-0.3000000"
--/
+/-- info: "-0.3000000" -/
+#guard_msgs in
 #eval (-0.3).toDecimal 7
 
-/-
-"-1"
--/
+/-- info: "-1" -/
+#guard_msgs in
 #eval (-1.23).toDecimal 0
