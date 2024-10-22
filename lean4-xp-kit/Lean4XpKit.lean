217ffba22f7f24a2e9a34534ad77bd894c27fe44
utensil <utensilcandel@gmail.com>
2024-10-22 17:17:45 +0800

    Run `lake +leanprover/lean4:v4.11.0 new lean4-xp-kit`, clean up, and build

---
+++ lean4-xp-kit/Lean4XpKit.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Lean4XpKit` library.
+-- Import modules here that should be built as part of the library.
+import Lean4XpKit.Basic
---
+++ lean4-xp-kit/lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+open Lake DSL
+
+package "lean4-xp-kit" where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Lean4XpKit» where
+  -- add library configuration options here
+
