f74888ed653757f67f3feb95b12c0425a98b1a18
utensil <utensilcandel@gmail.com>
2024-10-23 18:26:26 +0800

    Code clean up

--- lean4-verso-xp/Main.lean
+++
@@ -1,4 +0,0 @@
-import VersoXp
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- lean4-verso-xp/VersoXp.lean
+++ lean4-verso-xp/VersoXp.lean
@@ -1,5 +1,3 @@
--- This module serves as the root of the `Lean4VersoXp` library.
--- Import modules here that should be built as part of the library.
 import Mathlib
 import Verso
 import VersoXp.Basic
