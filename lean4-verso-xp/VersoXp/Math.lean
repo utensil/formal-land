db9096b7f27a0ac8f5f3a0ede18eb4e36b97e77a
utensil <utensilcandel@gmail.com>
2024-10-27 22:28:52 +0800

    Workaround for multiline math

--- lean4-verso-xp/VersoXp/Math.lean
+++ lean4-verso-xp/VersoXp/Math.lean
@@ -1,6 +1,16 @@
+import Verso
 import VersoManual
 
 open Lean.MessageSeverity
+
+-- #check Verso.Doc.Inline.math
+-- #check Verso.Doc.MathMode
+
+open Verso Doc Elab in
+@[code_block_expander latex]
+def Manual.latex : CodeBlockExpander
+  | _args, str => do
+    return #[(← `(Doc.Block.para #[Doc.Inline.math Doc.MathMode.display s!"{$str}"]))]
 
 open Verso.Genre Manual
 
@@ -21,4 +31,10 @@ Display math:
 $$`E=mc^2`
 
 ❌ But no support for multiline math yet.
+
+The following is a workaround inspired by [lecopivo/scientific-computing-lean](https://github.com/lecopivo/scientific-computing-lean/blob/ae7f1f6359465687136a9e75e0c83a1ef19525c2/ScientificComputingInLean/Meta/Latex.lean):
+
+```latex
+f'(x) = \lim_{h → 0} \frac{f(x+h) - f(x)}{h}
+```
 
