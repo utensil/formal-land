61fa92173b7caa8ba065df2563abd709e4c6df12
utensil <utensilcandel@gmail.com>
2024-10-28 23:41:22 +0800

    Hide bottom toc, and improve math syntax [annotate]

--- lean4-verso-xp/VersoXp/Math.lean
+++ lean4-verso-xp/VersoXp/Math.lean
@@ -7,8 +7,8 @@ open Lean.MessageSeverity
 -- #check Verso.Doc.MathMode
 
 open Verso Doc Elab in
-@[code_block_expander latex]
-def Manual.latex : CodeBlockExpander
+@[code_block_expander math]
+def Manual.math: CodeBlockExpander
   | _args, str => do
     return #[(← `(Doc.Block.para #[Doc.Inline.math Doc.MathMode.display s!"{$str}"]))]
 
@@ -32,9 +32,9 @@ $$`E=mc^2`
 
 ❌ But no support for multiline math yet.
 
-The following is a workaround inspired by [lecopivo/scientific-computing-lean](https://github.com/lecopivo/scientific-computing-lean/blob/ae7f1f6359465687136a9e75e0c83a1ef19525c2/ScientificComputingInLean/Meta/Latex.lean):
+The following is a workaround inspired by [lecopivo/scientific-computing-lean](https://github.com/lecopivo/scientific-computing-lean/blob/ae7f1f6359465687136a9e75e0c83a1ef19525c2/ScientificComputingInLean/Meta/Latex.lean), and the syntax is the same as [one variant of GFM for math](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/writing-mathematical-expressions#writing-expressions-as-blocks):
 
-```latex
+```math
 f'(x) = \lim_{h → 0} \frac{f(x+h) - f(x)}{h}
 ```
 
